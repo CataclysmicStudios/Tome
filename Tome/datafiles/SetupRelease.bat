7fa1d18b492fbfc654dd88a90012f66b65708035
Dustin Nazelrod <Dustin.Nazelrod@gmail.com>
2026-01-28 14:58:40 -0500

    Unmodified Files Name Change

--- Tome/datafiles/SetupRelease.bat
+++ Tome/datafiles/SetupRelease.bat
@@ -1,3 +1,3 @@
-xcopy /F /Y "UnmodifiedFiles\config.js" "Tome\config.js"
-xcopy /F /Y "UnmodifiedFiles\tomeConfig.gml" "..\scripts\tomeConfig\tomeConfig.gml"
-xcopy /F /Y "UnmodifiedFiles\tomeDocSetup.gml" "..\scripts\tomeDocSetup\tomeDocSetup.gml"
+xcopy /F /Y "UnmodifiedFiles\_config.js" "Tome\config.js"
+xcopy /F /Y "UnmodifiedFiles\_tomeConfig.gml" "..\scripts\tomeConfig\tomeConfig.gml"
+xcopy /F /Y "UnmodifiedFiles\_tomeDocSetup.gml" "..\scripts\tomeDocSetup\tomeDocSetup.gml"
