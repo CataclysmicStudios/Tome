6db24c4b54f8b7968cc873b74d28dd0e92603631
Dustin Nazelrod <Dustin.Nazelrod@gmail.com>
2026-02-02 13:23:43 -0500

    02.02.2026
    Hot fix

--- Tome/datafiles/Tome/config.js
+++ Tome/datafiles/Tome/config.js
@@ -1,8 +1,8 @@
 const config = {
-    description:"Documentation for the Tome library",
-  name:"Tome",
-  favicon:"assets/docsIcon.png",
-  themeColor:"#11DD11",
-  latestVersion:"01-29-2026",
-  otherVersions:["04-10-2025","03-27-2025","11-20-2024","03-06-2024","02-16-2024","02-15-2024","Beta-1"],
+     latestVersion: "",
+   otherVersions: [],
+   description: "Documentation for Your Project",
+   favicon: "assets/docsIcon.png",
+   name: "Your Project",
+   themeColor: "#FFFFFF" ,
 };
